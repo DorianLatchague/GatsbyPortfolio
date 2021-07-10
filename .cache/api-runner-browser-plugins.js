6991df4bf42512a93ca72d1804caf590555d5549
DorianLatchague <dorian.latchague@gmail.com>
2021-07-10 13:54:41 -0400

    removed .cache

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-image/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
-      options: {"plugins":[],"trackingId":"278094664","head":false,"anonymize":false,"respectDNT":false,"exclude":[],"pageTransitionDelay":0,"enableWebVitalsTracking":false},
-    }]
